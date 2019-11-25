f306c6e5c44eb64a1bad9e0bf9f90ea5c60aeafb
Andrei Enache <andrei.enache.mail@gmail.com>
2019-11-25 23:22:31 +0200

    deployment!

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t aenache/multi-client:latest aenache/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aenache/multi-server:latest aenache/multi-client:$SHA -f ./server/Dockerfile ./server
+docker build -t aenache/multi-worker:latest aenache/multi-client:$SHA -f ./worker/Dockerfile ./worker
+
+docker push aenache/multi-client:latest
+docker push aenache/multi-server:latest
+docker push aenache/multi-worker:latest
+
+docker push aenache/multi-client:$SHA
+docker push aenache/multi-server:$SHA
+docker push aenache/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment client=aenache/multi-client:$SHA
+kubectl set image deployments/server-deployment server=aenache/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=aenache/multi-worker:$SHA
